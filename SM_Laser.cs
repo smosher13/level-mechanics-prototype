ac3ffd83dd07a6d06df916ed648cc0af9454914a
smosher13 <44777657+smosher13@users.noreply.github.com>
2018-11-05 09:38:47 -0500

    Scripts for project

---
+++ SM_Laser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SM_Laser : MonoBehaviour {
+
+    [SerializeField] private Transform s_Sight;
+    private LineRenderer laserSight;
+    private Transform target = null;
+    [SerializeField] private float distance;
+    [SerializeField] private LayerMask mask;
+    private int baseDamage = 50;
+
+    private void TargetSearch()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(s_Sight.position, s_Sight.forward);
+        if (Physics.Raycast(ray, out hit, distance, mask))
+        {
+           if (hit.collider.tag == "Player")
+            {
+                hit.collider.GetComponent<Health>().ApplyDamage(baseDamage);
+                
+            }
+        }
+    }
+
+    private void Start()
+    {
+        laserSight = s_Sight.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        TargetSearch();
+    }
+}
---
+++ SM_PressurePlate.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SM_PressurePlate : MonoBehaviour {
+
+    public int objNum = 0;
+    public bool isPressed = false;
+    public GameObject animator;
+    private Animator doorAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        doorAnim = animator.GetComponent<Animator>();
+
+    }
+
+    void ToggleDoorAnim()
+    {
+        if (isPressed == true)
+        {
+            if (doorAnim.GetBool("toggleOpen") == false)
+            {
+                doorAnim.SetBool("toggleOpen", true);
+            }
+        }
+
+        else
+        {
+            doorAnim.SetBool("toggleOpen", false);
+        }
+
+    }
+        void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<GravityGun>()== null && other.name == "CubeGravNullObj")
+        {
+
+            objNum++;
+            isPressed = true;
+            Debug.Log("The plate is activated");
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.GetComponent<GravityGun>()== null && other.name == "CubeGravNullObj")
+        {
+            objNum--;
+
+            if (objNum <= 0)
+            {
+                objNum = 0;
+                isPressed = false;
+                Debug.Log("The plate is no longer activated");
+            }
+        }
+    }
+
+    void Update()
+    {
+        ToggleDoorAnim();
+    }
+
+
+}
